02e789ab42b78167d862ccd6d5956ab3ec09635d
fenyadim <dimos9813@gmail.com>
2024-09-15 01:51:19 +0800

    Начал делать главную страницу

--- src/entities/home/ui/wrapper.tsx
+++ src/entities/home/ui/wrapper.tsx
@@ -4,8 +4,8 @@ import { Chart } from './chart';
 export const Wrapper = () => {
 	return (
 		<div className='relative'>
-			<Chart className='z-0' />
-			<AccentBlock className='absolute -bottom-20 -left-20 z-10' />
+			<Chart />
+			<AccentBlock className='absolute -bottom-20 -left-20' />
 		</div>
 	);
 };
